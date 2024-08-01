b6cb95841c9eefaa2cc3cd949c9dd0a3338848fd
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2024-08-02 01:30:34 +0200

    Works?

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,24 +1,17 @@
 package example;
-
-import org.w3c.dom.events.Event;
-
 import arc.*;
-import arc.func.Boolf;
-import arc.func.Cons;
 import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Fill;
 import arc.graphics.g2d.Lines;
-import arc.scene.ui.Dialog;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.graphics.Layer;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import mindustry.ui.Menus;
 import mindustry.entities.Units;
 import mindustry.Vars;
 import mindustry.game.Team;
@@ -26,7 +19,8 @@ import mindustry.game.Team;
 
 public class ExampleJavaMod extends Mod{
 
-    private Unit focusedUnit = null;
+    private Unit focusedEnemyUnit = null;
+    private Unit focusedTeamUnit = null;
 
     public ExampleJavaMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
@@ -35,44 +29,36 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
+                //show dialog upon startup
+                Vars.ui.announce("HP Bars mod: Nearest enemy and ally at mouse position will be shown.", 4f);
             });
         });
 
 
         Events.run(EventType.Trigger.update, () -> {
 
-            focusedUnit = Units.closest(Vars.player.team(), Vars.player.x, Vars.player.y, 100f, u -> u.team() == Team.sharded);
-            if (focusedUnit == null) {
-                drawHPBar(Vars.player.x, Vars.player.y, 10, 100);
-                return;
-            }
-
-
-            float x = focusedUnit.x;
-            float y = focusedUnit.y;
-
-            // Ensure HP bar is drawn with updated player position
-            drawHPBar(x, y, 59, 100);
+            focusedTeamUnit = Units.closest(Vars.player.team(), Vars.player.mouseX(), Vars.player.mouseY(), 100f, u -> u.team() == Team.sharded);
+            focusedEnemyUnit = Units.closestEnemy(Vars.player.team(), Vars.player.mouseX(), Vars.player.mouseY(), 100f, u -> u.team() != Team.sharded);
         });
 
 
         Events.run(EventType.Trigger.draw, () -> {
             Draw.z(Layer.overlayUI);
-            if(focusedUnit == null) return;
-            drawHPBar(focusedUnit.x, focusedUnit.y, 10, 100);
+            if(focusedTeamUnit != null) 
+            {
+                drawHPBar(focusedTeamUnit.x, focusedTeamUnit.y, focusedTeamUnit.health(), focusedTeamUnit.maxHealth(), Color.green);
+            }
+            
+            if(focusedEnemyUnit != null) 
+            {
+                drawHPBar(focusedEnemyUnit.x, focusedEnemyUnit.y, focusedEnemyUnit.health(), focusedEnemyUnit.maxHealth(), Color.red);
+            }
 
-            //Draw.rect(Core.atlas.find("example-java-mod-frog"), Vars.player.x, Vars.player.y);
             Draw.reset();
         });
     }
     
-    public void drawHPBar(float x, float y, float health, float maxHealth)
+    public void drawHPBar(float x, float y, float health, float maxHealth, Color color)
     {
         // calculate the x and y position of the HP bar
         float barX = x - 20;
@@ -85,9 +71,9 @@ public class ExampleJavaMod extends Mod{
         Draw.color(Color.darkGray);
         Fill.crect(barX, barY, barWidth, barHeight);
 
-        float healthPercent = 0.5f; // health / maxHealth;
+        float healthPercent = health / maxHealth;
         // Draw the current health
-        Draw.color(Color.scarlet);
+        Draw.color(color);
         Fill.crect(barX, barY, barWidth * healthPercent, barHeight);
 
         // Draw the border
